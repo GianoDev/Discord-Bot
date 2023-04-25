7ab53e2f2b8ab901dd57c19398f5640087b571ed
Benzo <85945745+GianoDev@users.noreply.github.com>
2023-04-25 04:38:03 -0700

    Update owner.js

--- src/commands/bot/owner.js
+++ src/commands/bot/owner.js
@@ -7,22 +7,22 @@ module.exports = async (client, interaction, args) => {
         thumbnail: client.user.avatarURL({ dynamic: true, size: 1024 }),
         fields: [{
             name: "👑┆Owner name",
-            value: `Corwin`,
+            value: `Reis`,
             inline: true,
         },
         {
             name: "🏷┆Discord tag",
-            value: `</Corwin>#0001`,
+            value: `reis#1337`,
             inline: true,
         },
         {
             name: "🏢┆Organization",
-            value: `CoreWare`,
+            value: `malWare`,
             inline: true,
         },
         {
             name: "🌐┆Website",
-            value: `[https://corwindev.nl](https://corwindev.nl)`,
+            value: `[https://reisdev.nl](https://reisdev.nl)`,
             inline: true,
         }],
         type: 'editreply'
